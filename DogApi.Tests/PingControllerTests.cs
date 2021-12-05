b1ee65fd25e6bbb3ea32959f0857b4c39842f09f
damr <damr18@gmail.com>
2021-12-04 20:41:38 -0600

    Test project created  and test for the PingController

---
+++ DogApi.Tests/PingControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using DogApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace DogsApi.Tests
+{
+    public class PingControllerTests
+    {
+        [Fact]
+        public void GetPing_Returns_Name_And_Version()
+        {
+            // Arrange
+            var controller = new PingController();
+
+            // Act
+            var actionResult = controller.Get();
+
+            //Assert
+
+            Assert.Equal(new ObjectResult(new { message = "Dogs house service. Version 1.0.1" }).ToString(), actionResult.ToString());
+
+        }
+    }
+}
